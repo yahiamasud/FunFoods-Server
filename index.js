55ac1c8d135dc48d4e49e91d4c7184d389206e1d
Yahia Masud <79189930+yahiamasud@users.noreply.github.com>
2023-05-03 22:59:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+const chefdata = require('./data/chafdata.json');
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+
+app.get('/chefdata', (req, res) => {
+  res.send(chefdata)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
